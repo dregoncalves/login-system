e811342640e237027bafc958b18e966d7497272a
drezolis <dregoncalves22@gmail.com>
2024-04-04 00:50:19 -0300

    Form de login

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Login</title>
+</head>
+
+<body>
+    <section class="vh-100">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-xl-10">
+                    <div class="card" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="a.jpg" alt="Pangaré" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                            </div>
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
+
+                                    <form class="justify-content-center">
+
+                                        <div class="form-outline mb-4">
+                                            <label class="form-label" for="">Login</label>
+                                            <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <label class="form-label">Senha</label>
+                                            <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                                        </div>
+
+
+                                        <div class="mb-4 d-flex ">
+                                            <button type="button" style="background-color: #CFB29F; margin-left: 25%;" class="btn btn-lg ">Login</button>
+                                            <button type="button" style="background-color: #F9E5D7; margin-left: 60px;" class="btn btn-lg pl-2">Cadastrar</button>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>

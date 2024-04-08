824241348ea71d5c8f095c0cf6cab3178b5e1fad
Felipe Warchelski <fewarchelski@gmail.com>
2024-04-07 22:00:52 -0300

    Update

--- menu.php
+++ menu.php
@@ -1,6 +1,12 @@
 <?php
 require('verifica.php');
-echo "Logado como: " . $_SESSION["nome_usuario"];
+if (isset($_SESSION["nome_usuario"])) {
+    // Transforma a primeira letra do nome em maiúscula
+    $nomeUsuario = ucfirst(strtolower($_SESSION["nome_usuario"]));
+
+    // Exibe a mensagem de boas-vindas com o nome do usuário formatado
+    echo "<h1>Bem-vindo " . $nomeUsuario . "!</h1>";
+}
 ?>
 
 <!DOCTYPE html>
@@ -9,20 +15,31 @@ echo "Logado como: " . $_SESSION["nome_usuario"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="menu.css">
     <title>Menu</title>
 </head>
 
 <body>
-    <br>
-    <a href="cadastro.php">Cadastrar</a>
-    <?php
-    if($_SESSION['nivel_usuario'] == 'ADM') {
-        echo '<br> <a href="relatorio.php">Relatório</a>';
-    }
-    ?>
-    <br>
-    <a href="logout.php">Sair</a>
+    <section class="page1">
+        <div class="acoes">
+            <div class="botao">
+                <a href="cadastro.php">Cadastrar</a>
+            </div>
+            <div class="botao">
+                <?php
+                if ($_SESSION['nivel_usuario'] == 'ADM') {
+                    echo '<a href="relatorio.php">Relatório</a> <br>';
+                }
+                ?>
+            </div>
+            <div class="botao">
+                <a href="logout.php">Sair</a>
+            </div>
+
+
+        </div>
+    </section>
+
 </body>
 
 </html>
